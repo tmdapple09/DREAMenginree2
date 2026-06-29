8ba0ddb0f98c53d7a2b6b94a3a473a27eb32b689
tmdapple09 <tmdapple09@gmail.com>
2026-06-29 05:52:22 -0700

    Delete 14_observability directory

--- 14_observability/metrics/labengin/components/engines/lab/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as LabEnginApp } from './dream.LabEnginApp';
-export { default as DataVizPanel } from './panels/dream.panel.DataVizPanel';
-export { default as ExperimentsPanel } from './panels/dream.panel.ExperimentsPanel';
-export { default as QuantumPanel } from './panels/dream.panel.QuantumPanel';
-
--- 14_observability/metrics/ui_shell/components/engines/portfolio/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PortfolioEnginApp } from './dream.PortfolioEnginApp';
-export { default as AssetsPanel } from './panels/dream.panel.AssetsPanel';
-export { default as OptimizePanel } from './panels/dream.panel.OptimizePanel';
-export { default as PortfolioQuantumPanel } from './panels/dream.panel.PortfolioQuantumPanel';
-
